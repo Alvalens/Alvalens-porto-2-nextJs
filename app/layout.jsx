5312bfe59ae095665f4b124ead2770070cf05660
Alvalen <alvalen.shafel04@gmail.com>
2023-09-12 09:15:57 +0700

    [feat] change metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
-import Icon from "@/public/img/icon.png";
+import Icon from "@/public/img/icon.ico";
 import "./nprogress.css";
 
 export const metadata = {
@@ -14,34 +14,48 @@ export const metadata = {
 	image: Icon,
 	author: "Alvalens",
 	keywords:
-		"alvalens, alvalen, alvalen shafel, shafel, alvalen shafelbilyunazra, alvalen shafel bilyunazra, bloodfallen, alvalen porto, porto alvalens, alvalens portofolio, web alvalen, alvalens web, alvalens website, website alvalens, alvalens web developer, web portofolio alvalens, alvalen profile, profil alvalen",
-	
+		"alvalens, alvalen, alvalen shafel, shafel, alvalen shafelbilyunazra, alvalen shafel bilyunazra, bloodfallen, alvalen porto",
+	siteUrl: "https://alvalens.vercel.app",
+	ico: Icon,
+	applicationName: "Alvalens",
+	keywords: [
+		"alvalens",
+		"alvalen",
+		"alvalen shafel",
+		"shafel",
+		"alvalen shafelbilyunazra",
+		"alvalen shafel bilyunazra",
+		"bloodfallen",
+		"alvalen porto",
+		"alvalen um",
+	],
+	author: "Alvalen Shafelbilyunazra",
+	authorDescription: "I'm a web developer and I'm passionate about it.",
+	openGraph: {
+		type: "website",
+		url: "https://www.alvalens.my.id",
+		title: "Alvalens | Portofolio",
+		description: "My name is Alvalens, This is my portofolio website.",
+		image: Icon,
+		siteName: "Alvalens",
+	},
+	icons: {
+		icon: "img/icon.ico",
+		appleIcon: Icon,
+		favicon32: Icon,
+		favicon16: Icon,
+	},
 };
 
 const TopProgressBar = dynamic(() => import("@/components/TopProgressbar"), {
 	ssr: false,
 });
 
-const Meta = () => (
-	<>
-		<meta name="description" content={metadata.description} />
-		<meta property="og:title" content={metadata.title} />
-		<meta property="og:description" content={metadata.description} />
-		<meta property="og:image" content={metadata.image} />
-		<meta property="og:type" content="website" />
-		<meta name="application-name" content="Alvalens | Portofolio" />
-		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta name="author" content="Alvalens" />
-		</>
-);
+
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<head>
-			<link rel="icon" href="/img/icon.png" sizes="any" />
-			<Meta />
-			</head>
 			<body>
 				
 				<TopProgressBar />
