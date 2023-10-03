7660ac71c050c1f208227a46415533b2cfafcbb2
Alvalen <alvalen.shafel04@gmail.com>
2023-10-03 14:03:35 +0700

    feat: add web analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 import "./nprogress.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
 	title: "Alvalens | Portofolio",
@@ -66,11 +67,11 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body>
-				
 				<TopProgressBar />
 				<Navbar />
 				{children}
 				<Chat />
+				<Analytics />
 			</body>
 		</html>
 	);
