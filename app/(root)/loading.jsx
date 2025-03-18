f1edb19b5afa64c43627179269ede24936077c47
Alvalens <alvalen.shafel04@gmail.com>
2025-03-18 12:36:50 +0700

    fix: simplify Loading component structure

--- app/(root)/loading.jsx
+++ app/(root)/loading.jsx
@@ -1,11 +1,7 @@
 export default function Loading() {
 	return (
-		<>
-			(
-			<div className="fixed top-0 left-0 flex justify-center items-center h-screen w-screen bg-gray-300 z-[999]">
-				<h1 className="text-4xl text-gray-900">Loading...</h1>
-			</div>
-			)
-		</>
+		<div className="fixed top-0 left-0 flex justify-center items-center h-screen w-screen bg-gray-300 z-[999]">
+			<h1 className="text-4xl text-gray-900">Loading...</h1>
+		</div>
 	);
 }
