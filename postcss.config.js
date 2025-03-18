553880a3e09fd5d7ca584e0b7ce684c6273eb7d3
Alvalens <alvalen.shafel04@gmail.com>
2025-03-18 11:31:18 +0700

    refactor: update all modules to latest version

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+const config = {
+	plugins: {
+		"@tailwindcss/postcss": {},
+	},
+};
+export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  important: true,
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			// ...existing code...
+		},
+	},
+	plugins: [],
+};
